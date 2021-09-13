62e66905a297e1555553441ba9a248cf6ac89bd5
Madison <madivallarino@gmail.com>
2021-09-13 18:10:54 -0400

    recipe data worked on, still need ingredients and steps

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,18 +3,20 @@ import LandingPage from './components/LandingPage';
 import React, { useEffect, useState } from 'react';
 import Navbar from './components/Navbar';
 
+
 function App() {
-  const [randomRecipe, setRandomRecipe] = useState([
-    
-  ])
+  
+//   const [randomRecipe, setRandomRecipe] = useState([])
 
 //   useEffect(()=> {
-//     fetch('https://api.spoonacular.com/recipes/random?number=1&apiKey=83125be0e73e44f79eadf0234bb150e7')
+//     fetch('https://api.spoonacular.com/recipes/random?number=1&apiKey=0e62ad5621ab4dddad3ee6e791fc9d6b')
 //     .then(resp => resp.json())
-//     .then(data => setRandomRecipe({
-//       image: data.recipes[0].image
-//     }))
+//     .then(data => console.log(data))
 // },[])
+
+
+
+  
   
   return (
     <div className="App">
