4c341630f157499877c9dff9d63e226f51de7fdb
Billy Baney <billybaneydev@gmail.com>
2021-09-13 14:18:32 -0500

    pushing up styling changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,30 +1,29 @@
 import './App.css';
 import LandingPage from './components/LandingPage';
 import React, { useEffect, useState } from 'react';
+import Navbar from './components/Navbar';
 
 function App() {
-  
   const [randomRecipe, setRandomRecipe] = useState([
     
   ])
 
-  useEffect(()=> {
-    fetch('https://api.spoonacular.com/recipes/random?number=1&apiKey=83125be0e73e44f79eadf0234bb150e7')
-    .then(resp => resp.json())
-    .then(data => setRandomRecipe({
-      title : data.recipes[0].title,
-      image: data.recipes[0].image,
-      sourceUrl : data.recipes[0].sourceUrl
-    }))
-},[])
+//   useEffect(()=> {
+//     fetch('https://api.spoonacular.com/recipes/random?number=1&apiKey=83125be0e73e44f79eadf0234bb150e7')
+//     .then(resp => resp.json())
+//     .then(data => setRandomRecipe({
+//       image: data.recipes[0].image
+//     }))
+// },[])
   
   return (
     <div className="App">
-      <h1>Hiya</h1>
-      <p> I added this </p>
-      <LandingPage randomRecipe={randomRecipe}/>
+      <Navbar />
+      {/* <LandingPage randomRecipe={randomRecipe}/> */}
+      <LandingPage />
     </div>
   );
 }
 
 export default App;
+
--- frontend/src/components/LandingPage.js
+++ frontend/src/components/LandingPage.js
@@ -2,22 +2,24 @@ import React, { useEffect, useState } from 'react';
 
 function LandingPage({randomRecipe}){
 
-
-    console.log(randomRecipe)
- 
-
- 
-
-
 return (
-    <>
-    <h1> HOWDY </h1> 
-<h2>{randomRecipe.title}</h2>
-<img src={randomRecipe.image}/>
-<button><a href={randomRecipe.sourceUrl}>Cook This</a></button>
- 
-
-    </>
+    <div className="container">
+        <div className="welcome">
+            <h2>Welcome to</h2>
+            <h1>From Our<br/>
+                <span>Kitchen</span><br/>
+                To Yours</h1>
+            <p><span>From Our Kitchen To Yours</span> is your one stop spot to find new, 
+                delicious, easy to follow recipes for you to make at home!
+            </p>
+        </div>
+        <div className='random'>
+            <img src="https://images.squarespace-cdn.com/content/v1/59f0e6beace8641044d76e9c/1590772020832-BNJKTLRU9SYQPH9YFXFX/meal%2Bpic%2B3%2Bfor%2Bhome%2Bpage.jpg?format=1500w"/>
+            <br/>
+            {/* <button><a href={randomRecipe.sourceUrl}>Cook This Meal</a></button> */}
+            <button>See Our Recipes</button>
+        </div>
+    </div>
 )
 }
 
