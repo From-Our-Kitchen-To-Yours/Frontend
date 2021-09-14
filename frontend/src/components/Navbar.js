cd74b745822df9b567788609a0a765bba5fab879
Billy Baney <billybaneydev@gmail.com>
2021-09-14 14:06:51 -0500

    finished adding styles to recipe pages and bringing in instructions and ingredients

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,13 +1,13 @@
 import React from 'react'
-
+import {Link} from 'react-router-dom'
 
 function Navbar() {
     return (
         <div className='navbar'>
             <ul>
-                <li>Home</li>
-                <li>Recipes</li>
-                <li>Blog</li>
+                <Link to='/' style={{ textDecoration: 'none' }}><li>Home</li></Link>
+                <Link to='/recipes' style={{ textDecoration: 'none' }}><li>Recipes</li></Link>
+                <Link to='/blogs' style={{ textDecoration: 'none' }}><li>Blog</li></Link>
             </ul>
         </div>
     )
--- frontend/src/components/RecipeCard.js
+++ frontend/src/components/RecipeCard.js
@@ -1,15 +1,17 @@
 import React, { useState } from 'react'
 import {Link} from 'react-router-dom'
 import RecipeDetails from './RecipeDetails'
+import '../App.css'
 
 function RecipeCard({id, title, vegan, vegetarian}) {
 
 
     return (
-        <div>
-            <Link to={`/recipes/${id}`}>{title}</Link>
+        <div className="recipes">
+            <h4>{title}</h4>
             {vegan ? <p>Vegan: ✔️</p> : <p>Vegan: ❌</p>}
             {vegetarian ? <p>Vegetarian: ✔️</p> : <p>Vegetarian: ❌</p>}
+            <Link to={`/recipes/${id}`}><button>Cook This</button></Link>
         </div>
     )
 }
