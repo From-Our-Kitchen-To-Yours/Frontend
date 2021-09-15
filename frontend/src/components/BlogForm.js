edccfa96aa1b0651fb04cbe1b514ef446323332c
Madison <madivallarino@gmail.com>
2021-09-15 09:08:32 -0400

    blog posts to server, does not render to page yet

--- frontend/src/components/BlogForm.js
+++ frontend/src/components/BlogForm.js
@@ -1,4 +1,39 @@
+import React, { useEffect, useState } from 'react';
+import { v4 as uuid } from "uuid";
+
 function BlogForm() {
+    const [blogPage, setBlogPage] = useState([])
+    const [name, setUserName] = useState("");
+    const [image, setUserImage] = useState("");
+    const [blogText, setBlogText] = useState("");
+
+    useEffect(()=> {
+        fetch('http://localhost:3000/blogs')
+        .then((r)=> r.json())
+        .then(setBlogPage)
+    })
+
+    function handleSubmit(event){
+        event.preventDefault()
+        const newBlog = {
+            id: uuid(),
+            name,
+            image,
+            blogText,
+        }
+        fetch('http://localhost:3000/blogs',{
+            method: "POST",
+            headers: {
+                "Content-Type" : "application/json"
+           }, body:JSON.stringify(newBlog)
+        }).then((r)=> r.json())
+        .then(setBlogPage([newBlog]))
+        setUserName("");
+        setUserImage("");
+        setBlogText("");
+        
+    }
+
     return (
              <div>
             <div className="top-bar">
@@ -6,11 +41,11 @@ function BlogForm() {
                 <h4>Blog Posts</h4>
             </div>
             <h3> Share Your Story!</h3>
-            <form>
-                <input type="text" placeholder="Your Name"/> <br/>
-                <input type="text" placeholder="Share A Pic"/> <br/>
-                <input className='blog-post-textbox' type="text" placeholder="...Your Story Here"/><br/>
-                <input type="submit" value="Add Your Post"/>
+            <form onSubmit={handleSubmit}>
+                <input type="text" placeholder="Your Name" value={name} onChange={(e)=> setUserName(e.target.value)}/> <br/>
+                <input type="text" name="image" placeholder="Share A Pic" value={image} onChange={(e)=> setUserImage(e.target.value)}/> <br/>
+                <input className='blog-post-textbox' type="text" placeholder="...Your Story Here" value={blogText} onChange={(e)=> setBlogText(e.target.value)}/><br/>
+                <input type="submit" value="Add Your Post" onClick={handleSubmit}/>
             </form>
         </div>
     );
